bc489a06a5f32eb2313e52363ef1ccd537052da6
Gangeshwar Krishnamurthy <gangeshwark@gmail.com>
2017-05-07 13:44:13 +0530

    Model update

--- model.py
+++ model.py
@@ -30,7 +30,10 @@ class AspectLevelModel():
         self.__init_graph__()
 
     def __init_graph__(self):
-        self._init_placeholders()
+        if self.debug:
+            self._init_debug_inputs()
+        else:
+            self._init_placeholders()
 
         self._init_train_connectors()
         self._init_word_embeddings()
@@ -48,11 +51,15 @@ class AspectLevelModel():
              [7, 6, 0],
              [0, 7, 0]]
         xl = [2, 3, 1]
+        a = [5, 6, 7]
+        y = [[1], [0], [1]]
+        yl = [1, 1, 1]
         self.inputs = tf.constant(x, dtype=tf.int32, name='inputs')
+        self.input_aspect = tf.constant(a, dtype=tf.int32, name='input_aspect')
         self.inputs_length = tf.constant(xl, dtype=tf.int32, name='inputs_length')
 
-        self.targets = tf.constant(x, dtype=tf.int32, name='targets')
-        self.targets_length = tf.constant(xl, dtype=tf.int32, name='targets_length')
+        self.targets = tf.constant(y, dtype=tf.int32, name='targets')
+        self.targets_length = tf.constant(yl, dtype=tf.int32, name='targets_length')
 
     def _init_placeholders(self):
         """ Everything is time-major """
@@ -126,9 +133,9 @@ class AspectLevelModel():
                 dtype=tf.float32)
 
             self.input_aspect_embedded = tf.nn.embedding_lookup(
-                self.aspect_embedding_matrix, self.input_aspect)
-
-            self.input_aspect_embedded_final = [self.input_aspect_embedded for x in self.inputs_length]
+                self.aspect_embedding_matrix, self.input_aspect)  # -> [batch_size, da]
+            self.input_aspect_embedded_final = tf.tile(self.input_aspect_embedded,
+                                                       (1, self.outputs.shape[1], 1))  # -> [batch_size, N, da]
 
     def _init_word_embeddings(self):
         with tf.variable_scope("WordEmbedding") as scope:
@@ -143,41 +150,54 @@ class AspectLevelModel():
                 dtype=tf.float32)
 
             self.inputs_embedded = tf.nn.embedding_lookup(
-                self.embedding_matrix, self.inputs)
+                self.embedding_matrix, self.inputs)  # -> [batch_size, N, dw]
 
-            self.inputs_embedded_final = tf.concat([self.inputs_embedded, self.input_aspect_embedded_final], 1)
+            self.inputs_embedded_final = tf.concat([self.inputs_embedded, self.input_aspect_embedded_final],
+                                                   2)  # -> [batch_size, N, dw+da]
 
     def _init_simple(self):
         with tf.variable_scope("RNN") as scope:
             def output_fn(outputs):
-                return tf.contrib.layers.linear(outputs, self.vocab_size, scope=scope)
+                return tf.contrib.layers.fully_connected(outputs, self.vocab_size, scope=scope)
+
             # shape of state is [batch_size, cell.state_size]
             (self.outputs, self.state) = (
                 tf.nn.dynamic_rnn(cell=self.cell,
                                   inputs=self.inputs_embedded_final,
                                   sequence_length=self.inputs_length,
-                                  time_major=True,
                                   dtype=tf.float32)
             )
 
             """Not yet implemented - Does not work"""
             Wh = tf.Variable(
                 tf.random_normal(shape=[self.hidden_size, self.hidden_size], stddev=1.0 / tf.math.sqrt(600)),
-                dtype=tf.float32)
+                dtype=tf.float32)  # -> [d, d]
             Wv = tf.Variable(tf.random_normal(shape=[self.aspect_embedding_size, self.aspect_embedding_size],
-                                              stddev=1.0 / tf.math.sqrt(600)), dtype=tf.float32)
+                                              stddev=1.0 / tf.math.sqrt(600)), dtype=tf.float32)  # -> [da, da]
 
-            w = tf.Variable(tf.random_normal(shape=[self.hidden_size+self.aspect_embedding_size, 1],
-                                              stddev=1.0 / tf.math.sqrt(600)), dtype=tf.float32)
+            w = tf.Variable(tf.random_normal(shape=[self.hidden_size + self.aspect_embedding_size, 1],
+                                             stddev=1.0 / tf.math.sqrt(600)), dtype=tf.float32)  # -> [d+da, 1]
 
-            a = tf.stack([tf.matmul(Wh, state) for state in self.state.h])
+            H = tf.reshape(self.outputs, [-1, self.hidden_size])  # -> [batch_size x N, d]
+            a_ = tf.matmul(H, Wh)  # -> [batch_size x N, d]
+            a = tf.reshape(a_, tf.shape(self.outputs))  # -> [batch_size, N, d]
 
-            b = tf.stack([tf.matmul(Wv, self.input_aspect_embedded) for x in self.inputs_length])
-            M = tf.tanh(tf.concat([a, b], 0))
+            # a = tf.stack([tf.matmul(Wh, tf.transpose(state)) for state in self.outputs])
 
-            alpha = tf.nn.softmax(tf.matmul(tf.transpose(w), M))
+            # input_aspect_embedded shape is [batch_size, da]
+            b_ = tf.matmul(self.input_aspect_embedded, Wv)  # [batch_size, da] X [da, da] -> [batch_size, da]
+            b = tf.tile(b_, (1, self.outputs.shape[1], 1))  # [batch_size, N, da]
 
-            r = tf.matmul(self.state.h, tf.transpose(alpha), name='sentence_weighted_representation')
+            # b = tf.stack([tf.matmul(Wv, self.input_aspect_embedded) for x in self.inputs_length])
+            M = tf.tanh(tf.concat([a, b], 2))  # -> [batch_size, N, d+da]
+            M_ = tf.reshape(M, [-1, M.shape[2]])  # -> [batch_size x N, d+da]
+
+            alpha_ = tf.nn.softmax(tf.matmul(M_, w))  # -> [batch_size x N, 1]
+            alpha = tf.reshape(alpha_, [-1, self.outputs.shape[1], 1])  # -> [batch_size, N, 1]
+
+            # [batch_size, N, d] x [batch_size, N, 1]
+            r = tf.matmul(tf.transpose(self.outputs, [0, 2, 1]), alpha,
+                          name='sentence_weighted_representation')
 
             Wp = tf.Variable(
                 tf.random_normal(shape=[self.hidden_size, self.hidden_size], stddev=1.0 / tf.math.sqrt(600)),
@@ -187,21 +207,21 @@ class AspectLevelModel():
                 tf.random_normal(shape=[self.hidden_size, self.hidden_size], stddev=1.0 / tf.math.sqrt(600)),
                 dtype=tf.float32)
 
-            h_star = tf.tanh(tf.add(tf.matmul(Wp, r), tf.matmul(Wx,self.state.h)), name='sentence_representation')
+            # -> ([batch_size, d x 1] x [d, d])  + ([batch_size, d] x [d, d]) = [batch_size, d]
+            r_ = tf.reshape(r, [r.shape[0], -1])
+            h_star = tf.tanh(tf.add(tf.matmul(r_, Wp), tf.matmul(self.state.h, Wx)), name='sentence_representation')
 
             Ws = tf.Variable(
                 tf.random_normal(shape=[self.hidden_size, self.hidden_size], stddev=1.0 / tf.math.sqrt(600)),
                 dtype=tf.float32)
 
-            bs = tf.Variable(tf.zeros(shape=[self.hidden_size]))
+            bs = tf.Variable(tf.zeros(shape=[1, self.hidden_size, 1]))
 
-            e = tf.matmul(Ws, h_star)+bs
+            e = tf.contrib.layers.linear
 
-            self.logits_train = tf.nn.softmax(e)
+            self.y = tf.nn.softmax(tf.add(tf.reshape(tf.matmul(h_star, Ws), [h_star.shape[0], -1, 1]), tf.tile(bs, (h_star.shape[0],1,1))))
 
-
-
-            #self.logits_train = output_fn(self.outputs)
+            self.logits_train = output_fn(self.outputs)
             self.prediction_train = tf.argmax(self.logits_train, axis=-1,
                                               name='prediction_train')
 
@@ -252,6 +272,7 @@ class AspectLevelModel():
 
         # self.loss = seq2seq.sequence_loss(logits=logits, targets=targets, weights=self.loss_weights)
         self.train_op = tf.train.AdamOptimizer().minimize(self.loss)
+
     """
     def make_train_inputs(self, input_seq, target_seq):
         inputs_, inputs_length_ = helpers.batch(input_seq)
